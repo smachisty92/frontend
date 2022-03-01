7db975a412067fabcab57d0204f69bf86a791dbd
csyed <smachisty92@gmail.com>
2022-03-01 19:35:17 +0530

    terraform

--- mutable/env-dev/main.tf
+++ mutable/env-dev/main.tf
@@ -2,6 +2,8 @@ module "ec2" {
   source = "git::https://github.com/smachisty92/terraform-mutable-ec2"
   SPOT_INSTANCE_COUNT = var.SPOT_INSTANCE_COUNT
   OD_INSTANCE_COUNT = var.OD_INSTANCE_COUNT
+  SPOT_INSTANCE_TYPE = var.SPOT_INSTANCE_TYPE
+  OD_INSTANCE_TYPE = var.OD_INSTANCE_TYPE
 }
 
 #module "alb" {
