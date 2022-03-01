f922305c13d037bfc0f3bdefbe6f9c0e88fc89e4
csyed <smachisty92@gmail.com>
2022-03-01 17:32:53 +0530

    terraform

--- mutable/env-prod/backend.tf
+++ mutable/env-prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform62"
-    key    = "mutable/frontend/dev/terraform.tfstate"
+    key    = "mutable/frontend/prod/terraform.tfstate"
     region = "us-east-1"
   }
 }
